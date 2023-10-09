72a206bdce564678578f97fbf60d5c4001886a36
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2023-10-09 20:00:15 +0530

    Update config.py
    uninstall vars

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("5896130808:AAF-BA_3_NcbPCc9CgbMwSUV8_NuvbU8Gmg")
-APP_ID = int(os.environ.get("23267035"))
-API_HASH = os.environ.get("a516af48bca80b9f339980650126149c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+APP_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
 
 youtube_next_fetch = 0  # time in minute
 
